b6653c2593ea6219b4fd26fe4efac4d98f26b9cf
Azizo27 <78748936+Azizo27@users.noreply.github.com>
2023-05-09 12:43:19 +0200

    feat: Data Cleaning/ SSMS Connection

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-import sqlalchemy
+import time
+from IncomePrediction import predict_income
+from NeuralNetworkIncome import NeuralNetworkGrossIncome
 
 #This function loads the data ENTIRELY from a csv. Also, It can be filtered by date May/June. Also, It can copy to a new csv file by renaming the columns
 def LoadCsv(file_name, new_file_name):
@@ -99,7 +101,7 @@ def DisplayInformation(df):
 
 
 #This function cleans the data.
-#We send a chunk of data to this function, and it cleans it.
+#NB: I added "U" for Unknown to fill some null columns
 def CleaningData(df):
     
     print("Cleaning Date...")
@@ -108,63 +110,108 @@ def CleaningData(df):
     print("Nothing To Clean In Customer Code...")
     
     print("Cleaning Employee Index...")
+    #NB: In the original instruction, Employee Index should have five types: A, B, F, N, P. 
+    #    However, this column only have does not have P values but it does have S
     df.dropna(subset=['employee_index'], inplace=True)
     
     print("Nothing To Clean In Country Residence...")
     
     print("Cleaning Gender...")
-    df.dropna(subset=['employee_index'], inplace=True)
+    df['gender'].fillna('U', inplace=True)
+    
     
     print("Cleaning Age...")
+    df['age'] = pd.to_numeric(df['age'], errors='coerce')
+    df["age"] = np.where(df["age"] > 100, 100, df["age"])
+    df["age"] = np.where(df["age"] < 0, 0, df["age"])
     
     print("Cleaning Customer Start Date...")
+    # NB: How to fill the null values in this column?
+    df["customer_start_date"].fillna(pd.NaT, inplace=True)
+    df["customer_start_date"] = pd.to_datetime(df["customer_start_date"],format="%Y-%m-%d")
     
     print("Cleaning New Customer Index...")
+    #NB: At this step, There is no null values in this column 
+    # but I  do a fillna just in case.
+    df['new_customer_index'].fillna(0, inplace=True)
+    df['new_customer_index'].astype(int, inplace=True)
     
     print("Cleaning Customer Seniority...")
     df['customer_seniority'] = pd.to_numeric(df['customer_seniority'], errors='coerce')
     df["customer_seniority"] = np.where(df["customer_seniority"] < 0, 0, df["customer_seniority"])
+    df["customer_seniority"] = np.where(df["customer_seniority"] > 1000, 1000, df["customer_seniority"])
     df['customer_seniority'].fillna(0, inplace=True)
     
     print("Cleaning Primary Customer Index...")
+    # NB: Even If there is no null values, How to fill the null values in this column? (probably put it at 0)
+    df['primary_customer_index'].astype(int, inplace=True)
     
-    print("Cleaning Last Date as Primary Customer...")
+    
+    print("Cleaning Last Date as Primary Customer ...")
+    # NB: How to fill the null values in this column?
+    df["last_date_as_primary_customer"].fillna(pd.NaT, inplace=True)
+    df["last_date_as_primary_customer"] = pd.to_datetime(df["last_date_as_primary_customer"],format="%Y-%m-%d")
+    
     
     print("Cleaning Customer Type at Beginning of Month...")
+    df['customer_type_at_beginning_of_month'] = df['customer_type_at_beginning_of_month'].replace({'1.0': '1', 1.0: '1', 1: '1',
+                                                                                               '2.0': '2', 2.0: '2', 2: '2',
+                                                                                               '3.0': '3', 3.0: '3', 3: '3',
+                                                                                               '4.0': '4', 4.0: '4', 4: '4'})
+    df['customer_type_at_beginning_of_month'].fillna('U', inplace=True)
     
     print("Cleaning Customer Relation Type at Beginning of Month...")
+    #NB: There is a value "N" in this column, which is not in the original instruction
+    df['customer_relation_type_at_beginning_of_month'].fillna('U', inplace=True)
     
     print("Cleaning Residence Index...")
     
     print("Cleaning Foreigner Index...")
     
-    print("Cleaning Spouse Index...")
+    print("Cleaning Spouse Index !!!")
+    #NB: In the original instruction, Spouse Index should be equal to 1 if the customer is spouse of an employee. 
+    #    However, this column only have N/S values, for No and Si (Yes in Spanish).
+    df['spouse_index'].fillna('N', inplace=True)
     
     print("Cleaning Channel Used by Customer to Join...")
+    df['channel_used_by_customer_to_join'].fillna('UNK', inplace=True)
     
     print("Cleaning Deceased Index...")
     
     print("Cleaning Address Type...")
+    df['address_type'].fillna(0, inplace=True)
     
     print("Cleaning Province Code...")
+    df['province_code'].fillna(99, inplace=True)
     
     print("Cleaning Province Name...")
+    df['province_name'].str.replace(',', '', inplace=True)
+    df['province_name'].fillna('UNK', inplace=True)
     
     print("Cleaning Activity Index...")
+    #NB: At this step, There is no null values in this column. 
+    # All of it is equal to 1 but I do a fillna just in case
+    df['activity_index'].fillna(0, inplace=True)
+    df['activity_index'].astype(int, inplace=True)
     
     print("Cleaning Gross Income...")
+    #NB: Maybe do some supervised learning to fill the null values
+    median_by_province = df.groupby('province_code')['gross_income'].median()
+    df['gross_income'] = df.apply( lambda row: median_by_province[row['province_code']] 
+                                  if pd.isnull(row['gross_income']) else row['gross_income'], axis=1)
     
     print("Cleaning Segmentation...")
+    #NB: How to Fill the null values in this column?
     
     print("Cleaning All Products...")
+    df[[col for col in df.columns if col.startswith('product_')]].fillna(0, inplace=True)
+    df[[col for col in df.columns if col.startswith('product_')]].astype(int, inplace=True)
     
-    
-    df['age'] = pd.to_numeric(df['age'], errors='coerce')
-    df["customer_start_date"] = pd.to_datetime(df["customer_start_date"],format="%Y-%m-%d")
-    df["last_date_as_primary_customer"] = pd.to_datetime(df["last_date_as_primary_customer"],format="%Y-%m-%d")
-    df['customer_type_at_beginning_of_month'] = df['customer_type_at_beginning_of_month'].replace({'1.0': '1', 1.0: '1',
-                                                                                               '2.0': '2', 2.0: '2',
-                                                                                               '3.0': '3', 3.0: '3',
-                                                                                               '4.0': '4', 4.0: '4'})
-    
+    print("Done Cleaning Data !!")
     return df
+
+
+
+#TRY GOOGLE COLAB
+df = LoadCsv("Renamed_train_ver2.csv", "train.csv")
+DisplayInformation(df)
