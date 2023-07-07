c4a13179c6b602d3d3a9e4819dfa05810ab39abd
Azizo27 <78748936+Azizo27@users.noreply.github.com>
2023-07-07 14:04:52 +0200

    fix: result.html style

--- app.py
+++ app.py
@@ -29,14 +29,26 @@ def calculate_prediction(month, age, gross_income, customer_seniority, customer_
     }
     df = pd.DataFrame(data)
 
-    all_products = ["product_savings_account", "product_guarantees", "product_current_accounts"]
+    all_products=  [ "product_savings_account", "product_guarantees", "product_current_accounts",
+        "product_derivada_account", "product_payroll_account", "product_junior_account",
+        "product_mas_particular_account", "product_particular_account", "product_particular_plus_account",
+        "product_short_term_deposits", "product_medium_term_deposits", "product_long_term_deposits",
+        "product_e_account", "product_funds", "product_mortgage", "product_first_pensions",
+        "product_loans", "product_taxes", "product_credit_card", "product_securities",
+        "product_home_account", "product_payroll", "product_second_pensions", "product_direct_debit"]
 
     for target in all_products:
         target_column = PredictProbabilityProduct(df, target, month)
         df[target] = target_column
         print("Done with", target)
 
-    result = df.to_html()
+    columns_to_keep = df.columns[df.columns.str.startswith('product_')]
+    df = df[columns_to_keep]
+    
+    highest_values = df.iloc[0].sort_values(ascending=False)
+    df_ordered = df[highest_values.index]
+    
+    result = df_ordered.to_html()
     calculation_complete.set()  # Set the event to indicate that the calculation is complete
 
 
@@ -45,6 +57,8 @@ def calculate_prediction(month, age, gross_income, customer_seniority, customer_
 def show_result():
     global result
     if calculation_complete.is_set() and result is not None:
+        result = pd.read_html(result)[0] # Retransform the html table into a dataframe
+        result = result.drop("Unnamed: 0", axis=1)  # Drop the column that is created by default during the conversion
         return render_template('result.html', result=result)
     else:
         return render_template('waiting.html')
